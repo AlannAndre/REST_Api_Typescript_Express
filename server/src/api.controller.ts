57a1c25dd033dd4bba979b4405fb664537876333
AlannAndre <80alaand@skola.boras.se>
2022-08-19 11:41:38 +0200

    Clean api.controller and server.ts

--- server/src/api.controller.ts
+++ server/src/api.controller.ts
@@ -1,20 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 
-export function getOneObject (req: Request <{id: string}>, res: Response) {
-    console.log(req.params.id);
-    res.status(200).json("Hello World");
-};
-
-export function getAllObjects (req: Request, res: Response) {
-    res.status(200).json("Hello World");
-};
-
-export const saveObject = (req: Request, res: Response) => {
-    const data = req.body;
-    //TODO save to db or at least generate id and save to server
-    res.status(201).json(data);
-};
-
 export const logger = (req: Request, res: Response, next: NextFunction) => {
     console.log(req.method, req.path);
     next();
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,16 @@
 import express from "express";
-import {getAllObjects, getOneObject, logger, saveObject} from "./api.controller"
+import { booksRouter } from "./books/books.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
+import {logger} from "./api.controller"
 
 const app = express();
 app.use(logger);
 app.use(express.json());
-app.use(express.static("public"));
+app.use("/api/menu/books", booksRouter);
 
-app.get("/api", getAllObjects);
-app.get("/api/:id", getOneObject);
-app.post("/api", saveObject)
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 app.listen(3000,() => {
     console.log("server is running on http://localhost:3000");
