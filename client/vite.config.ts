1e2d5a24f2eb854c25e3475d3cd788e1ad292ef1
AlannAndre <80alaand@skola.boras.se>
2022-08-19 14:01:48 +0200

    proxy definition to avoid double "/api"

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,12 @@ import {defineConfig} from "vite";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000/api/menu/books",
+      /* "/api": "http://localhost:3000/api/menu/books", */
+      '/api': {
+        target: 'http://localhost:3000/api/menu/books/2',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     },
   },
 });
